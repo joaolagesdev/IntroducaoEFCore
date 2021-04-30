9fac9da5405a9dac0fd894d09c4e6d6fdcd65d4c
João <joaolages.dev@gmail.com>
2021-04-30 12:36:59 -0300

    Primeira Migração

--- Curso/Data/Configurations/PedidoConfiguration.cs
+++ Curso/Data/Configurations/PedidoConfiguration.cs
@@ -8,7 +8,7 @@ namespace CursoEFCore.Data.Configurations
     {
         public void Configure(EntityTypeBuilder<Pedido> builder)
         {
-            builder.ToTable("Produtos");
+            builder.ToTable("Pedidos");
             builder.HasKey(p => p.Id);
             builder.Property(p => p.IniciadoEm).HasDefaultValueSql("GETDATE()").ValueGeneratedOnAdd();
             builder.Property(p => p.Status).HasConversion<string>();
