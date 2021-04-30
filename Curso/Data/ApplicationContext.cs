acfc9fca783a4733f85fb2a42911d1165c7d3b04
João <joaolages.dev@gmail.com>
2021-04-30 14:04:37 -0300

    Inserindo registros

--- Curso/Data/ApplicationContext.cs
+++ Curso/Data/ApplicationContext.cs
@@ -1,5 +1,4 @@
-﻿using CursoEFCore.Data.Configurations;
-using CursoEFCore.Domain;
+﻿using CursoEFCore.Domain;
 using Microsoft.EntityFrameworkCore;
 
 namespace CursoEFCore.Data
@@ -7,6 +6,8 @@ namespace CursoEFCore.Data
     public class ApplicationContext : DbContext
     {
         public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data source=(localdb)\\mssqllocaldb;Initial Catalog=CursoEFCore; Integrated Security=true");
--- Curso/Program.cs
+++ Curso/Program.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CursoEFCore.Domain;
+using CursoEFCore.ValueObjects;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Linq;
 
@@ -8,15 +10,38 @@ namespace CursoEFCore
     {
         static void Main(string[] args)
         {
-            using var db = new Data.ApplicationContext();
-            // db.Database.Migrate();
-            var existeMigrationPendente = db.Database.GetPendingMigrations().Any();
-            Console.WriteLine("Curso EFCore!");
+            //using var db = new Data.ApplicationContext();
+            //// db.Database.Migrate();
+            //var existeMigrationPendente = db.Database.GetPendingMigrations().Any();
+            //Console.WriteLine("Curso EFCore!");
 
-            if (existeMigrationPendente)
+            //if (existeMigrationPendente)
+            //{
+            //    // Regra
+            //}
+            Console.WriteLine("\nCurso EFCore!");
+            InserirDados();
+        }
+
+        private static void InserirDados()
+        {
+            var produto = new Produto
             {
-                // Regra
-            }
+                Descricao = "Produto Teste",
+                CodigoBarras = "12345678",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MercadoriaParaRevenda,
+                Ativo = true
+            };
+            using var db = new Data.ApplicationContext(); // Todas as interações que ocorrem com o banco é através do contexto
+            // 4 opções, as indicadas são as duas primeiras
+            // db.Produtos.Add(produto); ou
+            // db.Set<Produto>().Add(produto); ou
+            // db.Entry(produto).State = EntityState.Added; ou
+            db.Add(produto);
+
+            var registros = db.SaveChanges(); // Persisti no banco de dados
+            Console.WriteLine($"Total Registro(s): {registros}");
         }
     }
 }
